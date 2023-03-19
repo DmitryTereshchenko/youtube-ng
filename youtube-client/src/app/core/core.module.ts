1d94d82c87db1ed2cb4497d49d1dae18482648ea
Dzmitry Tsiareshchanka <dima.tereshchenko.96@mail.ru>
2023-03-19 15:49:25 +0400

    feat: create header component

--- youtube-client/src/app/core/core.module.ts
+++ youtube-client/src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { SearchModule } from '../search/search.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,10 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     CommonModule,
+    SearchModule,
   ],
+  exports: [
+    HeaderComponent
+  ]
 })
 export class CoreModule { }
--- youtube-client/src/app/search/search.module.ts
+++ youtube-client/src/app/search/search.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchItemComponent } from './components/search-item/search-item.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
 
 @NgModule({
   declarations: [
     SearchItemComponent,
     SearchResultsComponent,
+    SearchFormComponent,
   ],
   imports: [
     CommonModule,
+  ],
+  exports: [
+    SearchFormComponent,
   ],
 })
 export class SearchModule { }
